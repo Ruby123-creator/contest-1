270c51291af910c935d9607941d24cce0600093b
Ruby123-creator <108233250+Ruby123-creator@users.noreply.github.com>
2023-01-07 16:32:11 +0000

    contest

--- script.js
+++ script.js
@@ -8,20 +8,51 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map(printingThroughMap);
+  function printingThroughMap(items){
+    if(items.profession==="developer"){
+      console.log(items);
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(printingThroughForeach);
+  function printingThroughForeach(items){
+    if(items.profession==="developer"){
+      console.log(items);
+    }
+  }
 }
 
 function addData() {
   //Write your code here
+  let new_item = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(new_item);
+  console.log(new_item);
 }
 
 function removeAdmin() {
   //Write your code here
+  let filtered_arr = arr.filter(function (val) {
+    //callback function
+    if (val.profession !== "admin") {
+      //filtering criteria
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, name: "Ruby Pal", age: "22", profession: "SDE" },
+    { id: 6, name: "Anish Kumar", age: "25", profession: "developer" },
+    { id: 7, name: "Ankit Dhiman", age: "21", profession: "Manager" },
+  ];
+ 
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
